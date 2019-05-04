2989a569469fcbf01458d3f3639de5f36bf2ecfe
Robustprogram <robustprogram@gmail.com>
2019-05-04 21:09:02 +1000

    Added a hover div that will be next to the finger when the user drags. Thus we are able to implement an indicator to dragging the course on mobile as drag and drop is not yet implemented in mobile

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import Vue2TouchEvents from 'vue2-touch-events'
 import App from './App.vue'
 
 import 'quasar-extras/roboto-font'
@@ -9,16 +10,18 @@ import {config} from './config'
 import './styles/quasar.styl'
 
 
+Vue.use(Vue2TouchEvents);
+
 Vue.use(Quasar, {
   config: {}
- })
+});
 
-Vue.use(VModal)
+Vue.use(VModal);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.prototype.$config = config
+Vue.prototype.$config = config;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
